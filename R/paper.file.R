881509b3144410836408cdc7d63db32a4bc94a88
Christopher Gillen <gillenc@kenyon.edu>
2020-08-12 13:36:36 -0400

    From R

---
+++ R/paper.file.R
@@ -0,0 +1,9 @@
+#' Text of Hebert paper Introduction
+#' P.D.N. Hebert, E.H. Penton, J.M. Burns, D.H. Janzen, W. Hallwachs, Ten species in one: DNA barcoding reveals cryptic species in the neotropical skipper butterfly Astraptes fulgerator, Proc. Natl. Acad. Sci. USA 101 (2004) 14812-14817.
+#'
+#' @format A text file
+#'
+#'
+#'
+#' @source \url{https://www.pnas.org/content/101/41/14812}
+"paper.file"
